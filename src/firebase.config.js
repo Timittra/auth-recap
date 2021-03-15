9b8c5d0fc392b71e1e74201882da42651238994b
Timittra Islam Hridi <timittra.cse.aust@gmail.com>
2021-03-15 22:22:45 +0600

    first commit

---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCwU20qM2Y8ioYbpXogWhLmgT9OugVbDpg",
+    authDomain: "auth-recap-by-hridi.firebaseapp.com",
+    projectId: "auth-recap-by-hridi",
+    storageBucket: "auth-recap-by-hridi.appspot.com",
+    messagingSenderId: "901480757908",
+    appId: "1:901480757908:web:8c545ca4da25f4506d9281"
+  };
+
+  export default firebaseConfig;
